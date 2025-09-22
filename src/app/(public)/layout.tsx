530a0a5b3598d6c86d3bf697cc9294290b7ff2de
avdheshRajput <avdeshrajput925064@gmail.com>
2025-09-22 11:23:27 +0000

    also in navbar

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -7,9 +7,9 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col mx-[10%]">
       <SiteHeader />
-      <main className="flex-1 mx-[10%]">{children}</main>
+      <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
   );
